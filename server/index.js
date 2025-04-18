b4b670ad9f593602689c7b6c5868fe771b85a7ed
Rajib Salui <rajibsalui2003@gmail.com>
2025-04-18 16:19:02 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,14 @@ import  GenerateImageRouter  from "./routes/GenerateImage.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    process.env.CLIENT_URL,
+    'http://localhost:3000',
+    'https://ai-image-generator-client-git-main-rajib-saluis-projects.vercel.app' // Add your frontend domain
+  ],
+  credentials: true
+}));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
