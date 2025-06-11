09242b679aff6a0a7525064d327dc79290b16482
Rajib Salui <rajibsalui2003@gmail.com>
2025-06-11 09:03:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,13 +9,10 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: [
-    process.env.CLIENT_URL,
-    'http://localhost:3000',
-    'https://ai-image-generator-client-git-main-rajib-saluis-projects.vercel.app' // Add your frontend domain
-  ],
-  credentials: true
+  origin: '*',
+  credentials: false
 }));
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
